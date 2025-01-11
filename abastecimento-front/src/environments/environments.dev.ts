58dc94e1a2295292433e4825ca5ff5d40049fc7e
Marcelo_Macedo <marcelo_macedo01@hotmail.com>
2025-01-10 23:10:42 -0300

    Add Dockerfile for containerized build and production setup; create development environment configuration

---
+++ abastecimento-front/src/environments/environments.dev.ts
@@ -0,0 +1,8 @@
+/**
+ * Defines the environment configuration for the development environment.
+ * This configuration includes the API URL for the application.
+ */
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8088'
+};
